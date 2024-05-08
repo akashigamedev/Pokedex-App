9aca902775f3c9649f4ccf766339df946fd148a4
akashi <akashi.gamedev@gmail.com>
2024-05-08 22:58:39 +0530

    Pokemon Repository and Resource Class Implemented

--- app/src/main/java/com/akashi/pokedex/repository/PokemonRepository.kt
+++ app/src/main/java/com/akashi/pokedex/repository/PokemonRepository.kt
@@ -3,6 +3,7 @@ package com.akashi.pokedex.repository
 import com.akashi.pokedex.data.remote.PokeAPI
 import com.akashi.pokedex.data.remote.responses.pokemon.PokemonList
 import com.akashi.pokedex.data.remote.responses.pokemon_info.Pokemon
+import com.akashi.pokedex.utils.Resource
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
@@ -11,4 +12,21 @@ class PokemonRepository @Inject constructor(
     private val api: PokeAPI
 ) {
 
+    suspend fun getPokemonList(limit: Int, offset: Int): Resource<PokemonList> {
+        val response = try {
+            api.getPokemonList(limit, offset)
+        } catch (e: Exception) {
+            return Resource.Error("An error occurred in fetching data.")
+        }
+        return Resource.Success(response)
+    }
+
+    suspend fun getPokemonInfo(pokemonName: String): Resource<Pokemon> {
+        val response = try {
+            api.getPokemonInfo(pokemonName)
+        } catch (e: Exception) {
+            return Resource.Error("An error occurred in fetching data.")
+        }
+        return Resource.Success(response)
+    }
 }
